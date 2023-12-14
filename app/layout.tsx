9331c1e3dbd143291cb5c24cec57c7299f0c12e7
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2023-12-14 16:56:23 +0530

    smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-32`}
       >
