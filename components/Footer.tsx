43121a67ae89274ce3236c2b5a27ec9994f6e768
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-01-09 17:25:33 +0530

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-sm font-semibold">
+        &copy; {currentYear} made with 💛 by{" "}
+        <Link
+          href="https://www.linkedin.com/in/iamjabeed/"
+          className="hover:cursor-pointer hover:underline"
+        >
+          Jabeed
+        </Link>
+        . All rights reserved.
+      </small>
+      <p className="text-sm">
+        <span className="font-semibold">About this website:</span> built with
+        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email & Resend, Vercel hosting.
+      </p>
+    </footer>
+  );
+};
+export default Footer;
