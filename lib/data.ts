72a89e3de02ffed4c54dd9488f4663bc44c59718
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-01-10 17:34:17 +0530

    refactor: project data

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import luxehaven from "@/public/luxehaven.png";
+import filmagic from "@/public/filmagic.png";
+import vocalize from "@/public/vocalize.png";
 
 export const links = [
   {
@@ -62,25 +62,26 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "LUXEHAVEN",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Developed a groundbreaking e-commerce platform using MERN, ensuring a user-centric, secure, and highly efficient shopping experience",
+
+    tags: ["React", "Express.js", "MongoDB", "Tailwind", "Mongoose", "Redux"],
+    imageUrl: luxehaven,
   },
   {
-    title: "rmtDev",
+    title: "The Filmagic",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "Created a movie streaming app with real-time access to detailed information on movies, TV shows, cast, related videos, and insider details using TMDB API.",
+    tags: ["React", "SCSS", "React Router", "Redux", "TMDB API"],
+    imageUrl: filmagic,
   },
   {
-    title: "Word Analytics",
+    title: "Vocalize",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Created a React app with advanced speech recognition technology, providing users with an interactive and hands-free experience.",
+    tags: ["React", "HTML5", "Tailwind", "Framer"],
+    imageUrl: vocalize,
   },
 ] as const;
 
