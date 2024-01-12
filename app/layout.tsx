b48a85a99fe30014f34fdd50ce71da7e8e4e3932
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-01-12 16:46:21 +0530

    add: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,6 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#FAFAF9] text-gray-950 relative pt-[5rem] sm:pt-10 dark:bg-[#000] dark:text-white dark:text-opacity-80`}
       >
-        {/* <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div> */}
-
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#000]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#000]"></div>
         <ThemeContextProvider>
