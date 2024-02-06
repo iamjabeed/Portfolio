e04e861bc963fce03da83e2c5f7fba338a7192c3
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-06 17:49:42 +0530

    style: gradient adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#FAFAF9] text-gray-950 relative pt-20 dark:bg-[#000] dark:text-white dark:text-opacity-80`}
       >
-        <div className="bg-[#CDECFF] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#000]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#000]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#000]"></div>
+        <div className="bg-[#CDECFF] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#000]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
